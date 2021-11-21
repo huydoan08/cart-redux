4c3d297ef6eb1f447fc6830197b0889857ebe7bb
ADMIN\Admin <doanvanhuy268@gmail.com>
2021-11-22 00:02:01 +0700

    dfs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit'
 
 import reducers from "./reducers/index";
 
-const store = createStore(
-  reducers,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//   reducers,
+//   {},
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+const store = configureStore({reducer: reducers})
 
 export default store;
